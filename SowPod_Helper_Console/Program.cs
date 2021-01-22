a49a5ae9003382f1a8b4e065e75c49df66d00f04
ToddTeese <todd.teese@colorconsultancy.co.uk>
2021-01-22 09:49:55 +0000

    start of key handler

--- SowPod_Helper_Console/Program.cs
+++ SowPod_Helper_Console/Program.cs
@@ -6,7 +6,30 @@ namespace SowPod_Helper_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("SOWPOD Helper");
+            Console.WriteLine("Written By Todd Teese");
+            Console.ReadKey();
+            bool helperRunning = true;
+            while (helperRunning) {
+                PrintHelpText();
+                ConsoleKeyInfo keyPressed = Console.ReadKey(true);
+                Console.WriteLine(keyPressed.KeyChar);
+                if (keyPressed.KeyChar == 'q') {
+                    helperRunning = false;
+                }
+            }
+            Console.WriteLine("Thanks for Using the SowPod Helper");
+            Console.WriteLine("Enjoy the rest of your day");
+            Console.ReadKey();
+
+        }
+
+        static void PrintHelpText() {
+            Console.Clear();
+            Console.WriteLine("1. Validate a Word");
+            Console.WriteLine("2. Find a word using letters");
+            Console.WriteLine("Q. Quit");
+            Console.WriteLine(">>");
         }
     }
 }
